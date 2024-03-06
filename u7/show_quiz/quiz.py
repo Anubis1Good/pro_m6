59d6afc963fbffd0bda92a51fee250d83eb1ad47
Anubis1Good <vlad.polyakov.96@inbox.ru>
2024-03-06 14:57:02 +0300

    add ends lessons

--- u7/show_quiz/quiz.py
+++ u7/show_quiz/quiz.py
@@ -1,31 +1,20 @@
-from flask import Flask, session, request, redirect, url_for
+import os
+from flask import Flask, session, request, redirect, url_for, render_template
 from db_scripts import get_question_after, get_quises
- 
+
 def start_quis(quiz_id):
     '''создаёт нужные значения в словаре session'''
     session['quiz'] = quiz_id
     session['last_question'] = 0
- 
+
 def end_quiz():
     session.clear()
- 
+
 def quiz_form():
     ''' функция получает список викторин из базы и формирует форму с выпадающим списком'''
-    html_beg = '''<html><body><h2>Выберите викторину:</h2><form method="post" action="index"><select name="quiz">'''
-    frm_submit = '''<p><input type="submit" value="Выбрать"> </p>'''
-
-
-    html_end = '''</select>''' + frm_submit + '''</form></body></html>'''
-    options = ''' '''
     q_list = get_quises()
-    for id, name in q_list:
-        option_line = ('''<option value="''' +
-                        str(id) + '''">''' +
-                        str(name) + '''</option>
-                      ''')
-        options = options + option_line
-    return html_beg + options + html_end
-       
+    return render_template('first.html', q_list=q_list)
+
 def index():
     ''' Первая страница: если пришли запросом GET, то выбрать викторину, 
     если POST - то запомнить id викторины и отправлять на вопросы'''
@@ -38,7 +27,7 @@ def index():
         quest_id = request.form.get('quiz') # выбранный номер викторины 
         start_quis(quest_id)
         return redirect(url_for('test'))
- 
+
 def test():
     '''возвращает страницу вопроса'''
     # что если пользователь без выбора викторины пошел сразу на адрес '/test'? 
@@ -53,20 +42,23 @@ def test():
             session['last_question'] = result[0]
             # если мы научили базу возвращать Row или dict, то надо писать не result[0], а result['id']
             return '<h1>' + str(session['quiz']) + '<br>' + str(result) + '</h1>'
- 
+
 def result():
     end_quiz()
     return "that's all folks!"
- 
+
+folder = os.getcwd() # запомнили текущую рабочую папку
 # Создаём объект веб-приложения:
-app = Flask(__name__)  
-app.add_url_rule('/', 'index', index)   # создаёт правило для URL '/'
-app.add_url_rule('/index', 'index', index, methods=['post', 'get']) # правило для '/index' 
+app = Flask(__name__, template_folder=folder, static_folder=folder) # первый параметр - имя модуля
+                            # параметр с именем static_folder определяет имя папки, содержащей статичные файлы
+                            # параметр с именем template_folder определяет имя папки, содержащей шаблоны
+
+app.add_url_rule('/', 'index', index, methods=['post', 'get'])   # создаёт правило для URL '/'
 app.add_url_rule('/test', 'test', test) # создаёт правило для URL '/test'
 app.add_url_rule('/result', 'result', result) # создаёт правило для URL '/test'
 # Устанавливаем ключ шифрования:
 app.config['SECRET_KEY'] = 'ThisIsSecretSecretSecretLife'
- 
+
 if __name__ == "__main__":
     # Запускаем веб-сервер:
     app.run()
--- u7/solution/db_scripts.py
+++ u7/solution/db_scripts.py
@@ -1,23 +1,23 @@
 import sqlite3
 from random import randint
- 
+
 db_name = 'quiz.sqlite'
 conn = None
 cursor = None
- 
+
 def open():
     global conn, cursor
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
- 
+
 def close():
     cursor.close()
     conn.close()
- 
+
 def do(query):
     cursor.execute(query)
     conn.commit()
- 
+
 def clear_db():
     ''' убивает все таблицы '''
     open()
@@ -28,7 +28,7 @@ def clear_db():
     query = '''DROP TABLE IF EXISTS quiz'''
     do(query)
     close()
- 
+
 def create():
     open()
     cursor.execute('''PRAGMA foreign_keys=on''')
@@ -53,23 +53,23 @@ def create():
                 FOREIGN KEY (question_id) REFERENCES question (id) )'''
     )
     close()
- 
+
 def show(table):
     query = 'SELECT * FROM ' + table
     open()
     cursor.execute(query)
     print(cursor.fetchall())
     close()
- 
+
 def show_tables():
     show('question')
     show('quiz')
     show('quiz_content')
- 
+
 def add_questions():
     questions = [
         ('Сколько месяцев в году имеют 28 дней?', 'Все', 'Один', 'Ни одного', 'Два'),
-        ('Каким станет зелёный утёс, если упадет в Красное море?', 'Мокрым?', 'Красным', 'Не изменится', 'Фиолетовым'),
+        ('Каким станет зеленый утес, если упадет в Красное море?', 'Мокрым?', 'Красным', 'Не изменится', 'Фиолетовым'),
         ('Какой рукой лучше размешивать чай?', 'Ложкой', 'Правой', 'Левой', 'Любой'),
         ('Что не имеет длины, глубины, ширины, высоты, а можно измерить?', 'Время', 'Глупость', 'Море', 'Воздух'),
         ('Когда сетью можно вытянуть воду?', 'Когда вода замерзла', 'Когда нет рыбы', 'Когда уплыла золотая рыбка', 'Когда сеть порвалась'),
@@ -80,7 +80,7 @@ def add_questions():
     cursor.executemany('''INSERT INTO question (question, answer, wrong1, wrong2, wrong3) VALUES (?,?,?,?,?)''', questions)
     conn.commit()
     close()
- 
+
 def add_quiz():
     quizes = [
         ('Викторина 1', ),
@@ -91,7 +91,7 @@ def add_quiz():
     cursor.executemany('''INSERT INTO quiz (name) VALUES (?)''', quizes)
     conn.commit()
     close()
- 
+
 def add_links():
     open()
     cursor.execute('''PRAGMA foreign_keys=on''')
@@ -104,11 +104,11 @@ def add_links():
         conn.commit()
         answer = input("Добавить связь (y / n)?")
     close()
- 
- 
+
+
 def get_question_after(last_id=0, vict_id=1):
     ''' возвращает следующий вопрос после вопроса с переданным id
-    для первого вопроса передаётся значение по умолчанию '''
+    для первого вопроса передается значение по умолчанию '''
     open()
     query = '''
     SELECT quiz_content.id, question.question, question.answer, question.wrong1, question.wrong2, question.wrong3
@@ -117,11 +117,11 @@ def get_question_after(last_id=0, vict_id=1):
     AND quiz_content.id > ? AND quiz_content.quiz_id == ? 
     ORDER BY quiz_content.id '''
     cursor.execute(query, [last_id, vict_id] )
- 
+
     result = cursor.fetchone()
     close()
     return result 
- 
+
 def get_quises():
     ''' возвращает список викторин (id, name) 
     можно брать только викторины, в которых есть вопросы, но пока простой вариант '''
@@ -130,8 +130,29 @@ def get_quises():
     cursor.execute(query)
     result = cursor.fetchall()
     close()
-    return result
- 
+    return result 
+
+def check_answer(q_id, ans_text):
+    query = '''
+            SELECT question.answer 
+            FROM quiz_content, question 
+            WHERE quiz_content.id = ? 
+            AND quiz_content.question_id = question.id
+        '''
+    open()
+    cursor.execute(query, str(q_id))
+    result = cursor.fetchone()
+    close()    
+    # print(result)
+    if result is None:
+        return False # не нашли
+    else:
+        if result[0] == ans_text:
+            # print(ans_text)
+            return True # ответ совпал
+        else:
+            return False # нашли, но ответ не совпал
+
 def get_quiz_count():
     ''' необязательная функция '''
     query = 'SELECT MAX(quiz_id) FROM quiz_content'
@@ -139,9 +160,8 @@ def get_quiz_count():
     cursor.execute(query)
     result = cursor.fetchone()
     close()
-    return result
-    
- 
+    return result 
+
 def get_random_quiz_id():
     query = 'SELECT quiz_id FROM quiz_content'
     open()
@@ -151,8 +171,7 @@ def get_random_quiz_id():
     rand_id = ids[rand_num][0]
     close()
     return rand_id
-    
- 
+
 def main():
     clear_db()
     create()
@@ -168,3 +187,4 @@ def main():
     
 if __name__ == "__main__":
     main()
+
